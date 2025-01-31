82f1602c8b9aa775d3da021c939062a11457dd5d
Asy'syifa Shabrina Munir <131370960+syifm@users.noreply.github.com>
2025-01-31 16:29:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import re
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st  # Tambahkan ini
+import streamlit as st  
+from sklearn.metrics import classification_report, accuracy_score
 
 df=pd.read_csv('telkomsel.csv')
 df_tweet = pd.DataFrame(df[['full_text']])
@@ -78,3 +79,45 @@ plt.xlabel('Sentimen')
 plt.ylabel('Jumlah')
 
 st.pyplot(fig)
+
+# Baca data hasil prediksi
+data = pd.read_csv("y_test_pred.csv")
+y_test = data["y_test"]
+y_pred = data["y_pred"]
+
+# Menampilkan hasil evaluasi di Streamlit
+st.title("Evaluasi Model Naive Bayes")
+
+# Menampilkan Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+st.subheader("Accuracy:")
+st.write(f"{accuracy:.4f}")
+
+# Mengubah classification report menjadi DataFrame
+report_dict = classification_report(y_test, y_pred, output_dict=True)
+report_df = pd.DataFrame(report_dict).transpose()
+
+# Menampilkan classification report dalam bentuk tabel
+st.subheader("Classification Report:")
+st.dataframe(report_df)
+
+# Baca data hasil prediksi
+data = pd.read_csv("y_test_pred_knn.csv")
+y_test_knn = data["y_test_knn"]
+y_pred_knn = data["y_pred_knn"]
+
+# Menampilkan hasil evaluasi di Streamlit
+st.title("Evaluasi Model KNN")
+
+# Menampilkan Accuracy
+accuracy_knn = accuracy_score(y_test_knn, y_pred_knn)
+st.subheader("Accuracy:")
+st.write(f"{accuracy_knn:.4f}")
+
+# Mengubah classification report menjadi DataFrame
+report_dict_knn = classification_report(y_test_knn, y_pred_knn, output_dict=True)
+report_df_knn = pd.DataFrame(report_dict_knn).transpose()
+
+# Menampilkan classification report dalam bentuk tabel
+st.subheader("Classification Report:")
+st.dataframe(report_df_knn)

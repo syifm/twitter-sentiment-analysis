84222abd62508b67be7f77f73d90d7555053c0a9
Asy'syifa Shabrina Munir <131370960+syifm@users.noreply.github.com>
2025-01-31 11:28:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.metrics import classification_report, accuracy_score
 
 # Prediksi sudah dilakukan sebelumnya
-# y_pred = naivebayes.predict(X_test_tfidf)
-# accuracy = accuracy_score(y_test, y_pred)
-# report = classification_report(y_test, y_pred)
+y_pred = naivebayes.predict(X_test_tfidf)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
 
 # Menampilkan hasil evaluasi di Streamlit
 st.title("Evaluasi Model Naive Bayes")
